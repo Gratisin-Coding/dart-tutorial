6d987eeca69983de8f49291ea52a5d78e6327ab7
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-07-18 20:48:21 +0700

    Update with Variables video

---
+++ 2-variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Inisiasi variabel lewat var
+  var fullName = "Tjandra Darmo";
+  var myAge = 37;
+
+  // Inisiasi variabel dengan mendefinisikan tipe data
+  double myAge = 40.03;
+
+  print(myAge);
+}
